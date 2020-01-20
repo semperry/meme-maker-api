ad036c04b963a99cf3009d60117632c405d4e9f4
semperry <ryan.curtis88@gmail.com>
2020-01-20 10:07:29 -0700

    get all route created and tested

--- app.py
+++ app.py
@@ -53,8 +53,14 @@ def add_meme():
 
   return jsonify("MEME POSTED")
 
-  
 # GET
+@app.route("/memes", methods=["GET"])
+def get_memes():
+  all_memes = Meme.query.all()
+  result = memes_schema.dump(all_memes)
+
+  return jsonify(result)
+
 # PUT
 # DELETE
 
